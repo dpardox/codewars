0a9c1ffb0a3afba57cdba94f3e2ee74218abadfe
Steven Pardo <dspardo6@gmail.com>
2018-05-17 00:22:57 -0500

    Array Diff test enhancement

--- src/3-array-diff/solution.spec.js
+++ src/3-array-diff/solution.spec.js
@@ -3,11 +3,23 @@ describe('array_diff test', () => {
     expect(array_diff).toBeDefined();
   });
 
-  it('basic', () => {
+  it('should a was [1,2], b was [1], expected [2]', () => {
     expect(array_diff([1,2], [1])).toEqual([2]);
+  });
+
+  it('should a was [1,2,2], b was [1], expected [2,2]', () => {
     expect(array_diff([1,2,2], [1])).toEqual([2,2]);
+  });
+
+  it('should a was [1,2,2], b was [2], expected [1]', () => {
     expect(array_diff([1,2,2], [2])).toEqual([1]);
+  });
+
+  it('should a was [1,2,2], b was [], expected [1,2,2]', () => {
     expect(array_diff([1,2,2], [])).toEqual([1,2,2]);
+  });
+
+  it('should a was [], b was [1,2], expected []', () => {
     expect(array_diff([], [1,2])).toEqual([]);
   });
 });
