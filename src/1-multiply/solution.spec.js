3143a1e5de19ddf8126b62f057cc5cf11d915318
Steven Pardo <dspardo6@gmail.com>
2018-05-18 15:09:01 -0500

    Mejora de los test

--- src/1-multiply/solution.spec.js
+++ src/1-multiply/solution.spec.js
@@ -3,10 +3,23 @@ describe('Multiply test', () => {
     expect(multiply).toBeDefined();
   });
 
-  it('should multiply numbers', () => {
+  it('should a was 1, b was 1, expected 1', () => {
     expect(multiply(1, 1)).toEqual(1);
+  });
+
+  it('should a was 2, b was 1, expected 2', () => {
     expect(multiply(2, 1)).toEqual(2);
+  });
+
+  it('should a was 2, b was 2, expected 4', () => {
     expect(multiply(2, 2)).toEqual(4);
+  });
+
+  it('should a was 3, b was 5, expected 15', () => {
     expect(multiply(3, 5)).toEqual(15);
+  });
+
+  it('should a was -5, b was 5, expected -25', () => {
+    expect(multiply(-5, 5)).toEqual(-25);
   });
 });
--- src/2-complementary-dna/solution.spec.js
+++ src/2-complementary-dna/solution.spec.js
@@ -3,12 +3,27 @@ describe('Complementary DNA test', () => {
     expect(DNAStrand).toBeDefined();
   });
 
-  it('basic', () => {
+  it('should dna was AAAA, expected TTTT', () => {
     expect(DNAStrand('AAAA')).toEqual('TTTT');
+  });
+
+  it('should dna was ATTGC, expected TAACG', () => {
     expect(DNAStrand('ATTGC')).toEqual('TAACG');
+  });
+
+  it('should dna was GTAT, expected CATA', () => {
     expect(DNAStrand('GTAT')).toEqual('CATA');
+  });
+
+  it('should dna was AAGG, expected TTCC', () => {
     expect(DNAStrand('AAGG')).toEqual('TTCC');
+  });
+
+  it('should dna was CGCG, expected GCGC', () => {
     expect(DNAStrand('CGCG')).toEqual('GCGC');
+  });
+
+  it('should dna was GTATCGATCGATCGATCGATTATATTTTCGACGAGATTTAAATATATATATATACGAGAGAATACAGATAGACAGATTA, expected CATAGCTAGCTAGCTAGCTAATATAAAAGCTGCTCTAAATTTATATATATATATGCTCTCTTATGTCTATCTGTCTAAT', () => {
     expect(DNAStrand('GTATCGATCGATCGATCGATTATATTTTCGACGAGATTTAAATATATATATATACGAGAGAATACAGATAGACAGATTA')).toEqual('CATAGCTAGCTAGCTAGCTAATATAAAAGCTGCTCTAAATTTATATATATATATGCTCTCTTATGTCTATCTGTCTAAT');
   });
 });
