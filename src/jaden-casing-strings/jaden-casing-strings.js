b17b7f17cd6d05701d3f86c66212a4340d7af8ed
Donovan Pardo <dspardo6@gmail.com>
2022-06-17 10:39:48 -0500

    Jaden Casing String challenge

---
+++ src/jaden-casing-strings/jaden-casing-strings.js
@@ -0,0 +1,3 @@
+String.prototype.toJadenCase = function() {
+  return this.split(' ').map(w => w[0].toUpperCase() + w.slice(1)).join(' ');
+};
---
+++ src/jaden-casing-strings/jaden-casing-strings.spec.js
@@ -0,0 +1,10 @@
+fdescribe('Jaden Casing test', () => {
+  it('Functions should be defined', () => {
+    expect(String.prototype.toJadenCase).toBeDefined();
+  });
+
+  it('Should return', () => {
+    const str = `How can mirrors be real if our eyes aren't real`;
+    expect(str.toJadenCase()).toEqual(`How Can Mirrors Be Real If Our Eyes Aren't Real`);
+  });
+});
